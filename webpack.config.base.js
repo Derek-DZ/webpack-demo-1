87df070cf4eca33df2220df69fc58c73c1348507
Derek-DZ <a956997493@gmail.com>
2020-10-11 15:07:49 +0800

    update

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "index.[contentHash].js",
+    filename: "index.[contenthash].js",
   },
 
   plugins: [
